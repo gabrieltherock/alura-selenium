4b015cd10a53023dbe3638753a8b9b9bf6ecc870
gabrieltherock <gabrieldarocha2013@hotmail.com>
2022-05-29 13:03:05 -0300

    refatorando

--- src/test/java/br/com/gabriel/leilao/login/LoginTest.java
+++ src/test/java/br/com/gabriel/leilao/login/LoginTest.java
@@ -2,55 +2,50 @@ package br.com.gabriel.leilao.login;
 
 import br.com.gabriel.leilao.lance.LancesPage;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 class LoginTest {
 
-    private LoginPage paginaDeLogin;
+	private LoginPage paginaDeLogin;
 
-    @BeforeEach
-    public void beforeEach() {
-        this.paginaDeLogin = new LoginPage();
-    }
+	@BeforeEach
+	public void beforeEach() {
+		this.paginaDeLogin = new LoginPage();
+	}
 
-    @AfterEach
-    public void afterEach() {
-        this.paginaDeLogin.fechar();
-    }
+	@AfterEach
+	public void afterEach() {
+		this.paginaDeLogin.fechar();
+	}
 
-    @Test
-    void deveriaEfetuarLoginComDadosValidos() {
-        paginaDeLogin.preencherFormularioDeLogin("fulano", "pass");
-        paginaDeLogin.efetuarLogin();
+	@Test
+	void deveriaEfetuarLoginComDadosValidos() {
+		paginaDeLogin.efetuarLogin("fulano", "pass");
 
-        String nomeUsuarioLogado = paginaDeLogin.getNomeUsuarioLogado();
-        assertEquals("fulano", nomeUsuarioLogado);
-        assertFalse(paginaDeLogin.isPaginaAtual());
-    }
+		String nomeUsuarioLogado = paginaDeLogin.getNomeUsuarioLogado();
+		Assertions.assertEquals("fulano", nomeUsuarioLogado);
+		Assertions.assertFalse(paginaDeLogin.isPaginaAtual());
+	}
 
-    @Test
-    void naoDeveriaEfetuarLoginComDadosInvalidos() {
-        paginaDeLogin.preencherFormularioDeLogin("invalido", "1233");
-        paginaDeLogin.efetuarLogin();
+	@Test
+	void naoDeveriaEfetuarLoginComDadosInvalidos() {
+		paginaDeLogin.efetuarLogin("invalido", "1233");
 
-        assertNull(paginaDeLogin.getNomeUsuarioLogado());
-        assertTrue(paginaDeLogin.isPaginaAtual());
-        assertTrue(paginaDeLogin.isMensagemDeLoginInvalidoVisivel());
-    }
+		Assertions.assertNull(paginaDeLogin.getNomeUsuarioLogado());
+		Assertions.assertTrue(paginaDeLogin.isPaginaAtual());
+		Assertions.assertTrue(paginaDeLogin.isMensagemDeLoginInvalidoVisivel());
+	}
 
-    @Test
-    void naoDeveriaAcessarUrlRestritaSemEstarLogado() {
-        LancesPage paginaDeLances = new LancesPage();
+	@Test
+	void naoDeveriaAcessarUrlRestritaSemEstarLogado() {
+		LancesPage paginaDeLances = new LancesPage();
 
-        assertFalse(paginaDeLances.isPaginaAtual());
-        assertFalse(paginaDeLances.isTituloLeilaoVisivel());
+		Assertions.assertFalse(paginaDeLances.isPaginaAtual());
+		Assertions.assertFalse(paginaDeLances.isTituloLeilaoVisivel());
 
-        paginaDeLances.fechar();
-    }
+		paginaDeLances.fechar();
+	}
+
 }
